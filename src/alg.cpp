a63ac75f1e46502370e11fbf9de57f8a31ab08c6
Kstovchanin1902 <78904695+Kstovchanin1902@users.noreply.github.com>
2021-04-13 19:17:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,9 +2,9 @@
 
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i<(len-1); i++) {
+  for (int i = 0; i < (len-1); i++) {
       for (int j = i + 1; j < len; j++) {
-          if ((arr[i]+arr[j])==value) {
+          if ((arr[i] + arr[j]) == value) {
               count++;
           }
       }
@@ -18,9 +18,9 @@ int countPairs2(int *arr, int len, int value) {
     while ((arr[0]+arr[num])>value) {
         num--;
     }
-    for (int i = 0; i<(num+1); i++) {
+    for (int i = 0; i < (num+1); i++) {
         for (int j = i + 1; j< (num+1); j++) {
-            if ((arr[i]+arr[j])==value) {
+            if ((arr[i] + arr[j]) == value) {
                 count++;
             }
         }
@@ -29,34 +29,34 @@ int countPairs2(int *arr, int len, int value) {
 }
 
 int countPairs3(int *arr, int len, int value) {
-  int count=0, lborder=0, rborder=len, med=(lborder+rborder)/2, left=med, right=med;
-    for (int i=0; i<len; i++){
-      while (rborder>=lborder){
-        med=(lborder+rborder)/2;
-        left=med-1;
-        right=med+1;
-        if (arr[i]+arr[med]==value){
-          if (i!=med) {
+  int count = 0, lborder = 0, rborder = len, med = (lborder + rborder) / 2, left = med, right = med;
+    for (int i = 0; i < len; i++) {
+      while (rborder >= border) {
+        med = (lborder + rborder) / 2;
+        left = med - 1;
+        right = med + 1;
+        if ((arr[i] + arr[med]) == value) {
+          if (i != med) {
             count++;
           }
-          while (((arr[i]+arr[left])==value)&&(i!=left)){
+          while (((arr[i] + arr[left]) == value) && (i > left)) {
             count++;
             left--;
           }
-          while (((arr[i]+arr[right])==value)&&(i!=right)){
+          while (((arr[i] + arr[right]) == value) && (i < right)) {
             count++;
             right++;
           }
-          rborder=lborder-1;
-       } else if ((arr[i]+arr[med])<value){
-                lborder=med+1;
-              } else if ((arr[i]+arr[med])>value){
-                        rborder=med-1;
+          rborder = lborder-1;
+       } else if ((arr[i] + arr[med]) < value) {
+                lborder = med + 1;
+              } else if ((arr[i] + arr[med]) > value) {
+                       rborder = med - 1;
                      }
         }
-        arr[i]=value+1;
-        rborder=len;
-        lborder=0;
+        arr[i] = value + 1;
+        rborder = len;
+        lborder = i;
     }
     return count;
 }
